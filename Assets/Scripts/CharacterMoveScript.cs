7b49a912e4549e713aa59b814ef3f35bd7bcf928
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2024-03-02 17:23:23 +0000

    Quick Updates

---
+++ Assets/Scripts/CharacterMoveScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveScript : MonoBehaviour
+{
+    public Rigidbody2D myRb;
+    public float maxSpeed;
+    public float acceleration;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
