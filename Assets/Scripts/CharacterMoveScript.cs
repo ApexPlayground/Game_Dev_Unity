9dc06d902928940f045363dd5e46f14647020e1d
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2024-03-04 15:02:46 +0000

    Updated Material

--- Assets/Scripts/CharacterMoveScript.cs
+++ Assets/Scripts/CharacterMoveScript.cs
@@ -11,12 +11,15 @@ public class CharacterMoveScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Mathf.Abs(myRb.velocity.magnitude) < maxSpeed && (Mathf.Abs(Input.GetAxis("Horizontal")) > 0))
+        {
+            myRb.AddForce(new Vector2(acceleration * Input.GetAxis("Horizontal"),0), ForceMode2D.Force);
+        }
     }
 }
